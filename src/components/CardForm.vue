09a1a2f7fe5d3f027dc4cf0d5a5083df1aa5d970
Sebastian Alvarez <sleam91@gmail.com>
2020-05-18 21:14:26 +0200

    limit input text length

--- src/components/CardForm.vue
+++ src/components/CardForm.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="card-form">
         <p>CARD NUMBER</p>
-        <input v-model="card.cardNumber" @input="update()" />
+        <input v-model="card.cardNumber" @input="update()" maxlength="19"/>
         <p>CARD HOLDER NAME</p>
-        <input v-model="card.name" placeholder="FIRSTNAME LASTNAME" @input="update()" />
+        <input v-model="card.name" placeholder="FIRSTNAME LASTNAME" @input="update()" maxlength="24"  />
         <div class="date-ccv">
             <div>
                 <p>VALID THRU</p>
-                <input v-model="card.valThru" @input="update()" />
+                <input v-model="card.valThru" @input="update()" maxlength="5"/>
             </div>
             <div>
                 <p>CCV</p>
-                <input v-model="card.ccv" />
+                <input v-model="card.ccv" type="number"/>
             </div>
         </div>
         <p>VENDOR</p>
@@ -159,6 +159,7 @@ p {
         // width: 50%;
 
         p {
+            white-space: nowrap;
             // margin-left: 1.5rem;
         }
         input {
